f0dab2cac68fe39a46fd3cb9597e05e75d5c42c7
nareshkrish <naresh.ideas2it@gmail.com>
2015-11-13 19:37:28 +0530

    Basic Plans UI

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -1,13 +1,11 @@
-var visitorCenter = angular.module('VisitorCenter', ['ngResource']);
-
-visitorCenter.factory("Visitor", function($resource) {
+myApp.factory("Visitor", function($resource) {
   return $resource("visitors/:id", { id: '@id' }, {
     index:   { method: 'GET', isArray: true, responseType: 'json' },
     update:  { method: 'PUT', responseType: 'json' }
   });
 })
 
-visitorCenter.controller("visitorsController", function($scope, Visitor) {
+myApp.controller("visitorsController", function($scope, Visitor) {
   $scope.visitors = Visitor.index()
 
   $scope.addVisitor = function() {
